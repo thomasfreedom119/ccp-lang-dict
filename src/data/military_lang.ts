e7c0eb42d4f2c514a6aaaa44abfec7208ca0de8b
thomasfreedom119 <104294607+thomasfreedom119@users.noreply.github.com>
2022-06-07 15:22:11 +0800

    update military terms

--- src/data/military_lang.ts
+++ src/data/military_lang.ts
@@ -13,5 +13,1070 @@ export const milLangItems: LangItemType[] = [
     officialLang: '两海',
     normalLang: '表面迷惑对手以为是东海和南海，实际指东中国海和南中国海',
   },
+  {
+    officialLang: '国防公路',
+    normalLang: '根据国防需要，在国家公路干道网上选定的或专门修建的公路。是国防交通网的组成部分，对平时、战时的军事运输和国民经济建设具有重要作用。',
+    detail: ''
+  },
+  {
+    officialLang: '作战方法',
+    normalLang: '简称战法，组织与实施作战行动的方法。包括战役法和战术。作战方法随着军事技术和武器装备的发展而发展。',
+    detail: ''
+  },
+  {
+    officialLang: '地图仓库',
+    normalLang: '简称图库。储备、管理及供应军用地形图的仓库。',
+    detail: ''
+  },
+  {
+    officialLang: '军事制度',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '交通运输动员',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '主要进攻方向',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '辅助进攻方向',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '军事通信',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '基本指挥所',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '预备指挥所',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '总攻击',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '军事专题地图',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '机场平面图',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '输油管线部队',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '核爆炸装置',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '航空母舰',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '翼型伞',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '军事力量',
+    normalLang: '简称军力，国家或政治集团拥有的用于遂行军事任务的各种组织、人员及其武器装备等的统称',
+    detail: ''
+  },
+  {
+    officialLang: '海军船艺。舰艇操作、舱面作业、舰体保养和舱面通用设备维护等技艺的统称。',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '水障碍物',
+    normalLang: '简称水障。人为造成的泛滥地区和泥泞地段的统称。',
+    detail: ''
+  },
+  {
+    officialLang: '军用仓库',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '军队后勤',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '国防科学技术',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '舰艇损害管制',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '军事演习',
+    normalLang: '简称演习。部队在导演部组织和想定情况诱导下进行的作战指挥和行动的演练。按层次，分为战略演习、战役演习、战术演习；按对象，分为首长机关演习、实兵演习；按场所分为室内演习、现地演习；按形式，分为单方演习、对抗演习；按目的，分为检验性演习、研究性演习和示范性演习；还可按演习内容进行划分。是军事训练的高级形式。',
+    detail: ''
+  },
+  {
+    officialLang: '探雷针',
+    normalLang: '简称探针。以触及雷体的方式发现地雷的针状探雷装备。通常与单兵探雷器配合使用。作业时将探雷针插入土、雪等覆盖物中，凭手感发现地雷',
+    detail: ''
+  },
+  {
+    officialLang: '军事情报',
+    normalLang: '简称情报。对与国家安全、国家利益和军事斗争有关的情况和资料进行分析判断的成果。按使用范围，分为战略情报，战役情报和战术情报；按获取途径，分为公开情报和秘密情报；按获取手段，分为谍报侦查情报、技术侦查情报、武官侦查情报、部队侦查情报、航天侦查情报等。是筹划和指导战争、谋划国防和军队建设及指挥军事行动的重要依据，军队战斗力的构成要素。',
+    detail: ''
+  },
+  {
+    officialLang: '油料供应指标',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '竞争性装备采购',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '核反应堆',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '发射装药号数',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '地雷群',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '核毁伤估算',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '核闪光护目镜',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '舰队航空兵',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '飞行指挥塔台',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '导弹发射控制中心',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '舰艇舱面作业',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '末段制导炮弹',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '储备区',
+    normalLang: '预备役专业技术兵储备区的简称。国家按照军事专业技术种类划定的积蓄预备役兵员的区域',
+    detail: ''
+  },
+  {
+    officialLang: '通信计划',
+    normalLang: '通信保障计划的简称。为组织实施作战和其他军事任务的通信保障而制定的计划。是通信部队、分队行动的依据。',
+    detail: ''
+  },
+  {
+    officialLang: '无源干扰',
+    normalLang: '无源电子干扰的简称。使用本身不发射电磁波的器材反射或吸收地方发射的电磁波而形成的电子干扰。分为反射型无源干扰和吸收型无源干扰。',
+    detail: ''
+  },
+  {
+    officialLang: '军语',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '战区',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '歼驱',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '出击',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '指挥员',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '战役空降',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '协同',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '有源干扰',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '瞄准式干扰',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '观通站',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '通信指示',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '军队体制',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '军官',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '军人',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '导航干扰',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '军空点',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '炮空点',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '联勤',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '战略储备',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '末制导',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '炮兵区分',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '潜渡场',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '导爆管',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '受染区',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '自然干扰',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '兵力密度',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '密级',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '通信方案',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '网管网',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '观通站',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '反干扰训练',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '水船',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '油船',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '军港',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '海军旗',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '线导鱼雷',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '上浮水雷',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '雷达反侦察',
+    normalLang: '雷达反电子侦查的简称。为防止敌方截获和利用己方雷达信号而采取的战术技术措施及其行动的统称。',
+    detail: ''
+  },
+  {
+    officialLang: '反干扰',
+    normalLang: '反电子干扰的简称。为降低或消除地方电子干扰对己方电子信息设备和系统使用效能的影响而采取的措施及其行动的统称',
+    detail: ''
+  },
+  {
+    officialLang: '装备',
+    normalLang: '武器装备的统称。用于作战和保障作战及其他军事行动的武器、武器系统、电子信息系统和技术设备、器材等的统称。主要指武装力量编制内的舰艇、飞机、导弹、雷达、坦克、火炮、车辆和工程机械等。分为战斗装备、电子信息装备和保障装备。',
+    detail: ''
+  },
+  {
+    officialLang: '专业兵',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '指挥中心',
+    normalLang: '指挥控制中心的简称。军队各级指挥机构编组中的核心部门。具有对其他部门的指导协调职能。',
+    detail: ''
+  },
+  {
+    officialLang: '炮兵通控机',
+    normalLang: '炮兵通信控制机的简称。炮兵用于集中控制有线电通信和无线电通信的设备。',
+    detail: ''
+  },
+  {
+    officialLang: '指挥组',
+    normalLang: '指挥小组的简称。为加强指挥而临时派出的精干指挥机构',
+    detail: ''
+  },
+  {
+    officialLang: '捕俘',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '审俘',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '歼驱',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '防毒衣',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '防毒服',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '舰艇战位',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '合训分流',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '模型校验',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '反特站',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '招飞',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '空运',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '卫星轨道',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '军事挑衅',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '战略学',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '国防工业',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '航空管制',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '应急动员',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '义务兵役制',
+    normalLang: '亦称征兵制。公民在一定年龄内必须依法服兵役的制度。包括服现役和服预备役的制度等。',
+    detail: ''
+  },
+  {
+    officialLang: '太空战略',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '消极防御',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '战略退却',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '登陆作战',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '破袭战',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '歼灭',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '进攻方向',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '防空战役',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '领受任务',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '集中指挥',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '指挥权限',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '作战方案',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '首长决心',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '专项指挥所',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '人文地理环境',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '海滩',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '方位圈',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '航天员',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '训练形式',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '冲击雷达',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '游动哨',
+    normalLang: '亦称巡逻哨。在一定区域内担负巡逻警戒任务的哨兵。通常在警戒线附近或宿营地域内，按规定的范围或路线进行巡逻警戒。',
+    detail: ''
+  },
+  {
+    officialLang: '数字栅格地图',
+    normalLang: '亦称像素图。以二维像元阵列方式存储的数字地图。可在屏幕上显示为电子地图和作为计算机标图的底图。',
+    detail: ''
+  },
+  {
+    officialLang: '集团军群战役',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '战斗原则',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '太空战场',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '直射火力',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '正射火力',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '倒射火力',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '侧方阵地',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '战备计划',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '分布式干扰',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '检验性演习',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '示范性演习',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '太阳电磁辐射',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '装备经费',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '公路军事运输',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '国防经济',
+    normalLang: '亦称军事经济。为保障国家安全、满足军事需求而开展的经济活动及与此相适应的经济关系等等统称。是国民经济的组成部分。',
+    detail: ''
+  },
+  {
+    officialLang: '指挥对象',
+    normalLang: '亦称被指挥者。构成指挥关系的被指挥单位和人员的统称。',
+    detail: ''
+  },
+  {
+    officialLang: '道路标志',
+    normalLang: '亦称道路交通标志。设置在交通道路上，传递交通管理信息的文字、符号及其设备的统称。',
+    detail: ''
+  },
+  {
+    officialLang: '信息防御',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '电子防御',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '自然地理环境',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '海道测量',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '移动设备',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '武器',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '国际人道法',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '轻武器',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '炮兵标准射击条件',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '火具',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '舰艇',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '水中武器',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '舰艇雷达',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '海岸雷达',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '舰艇电子对抗装备',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '舰艇和生化武器防护装备',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '航空机务人员',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '军事干涉',
+    normalLang: '亦称武装干涉。国家或国际组织使用武装力量强行介入某国内部事务的行为',
+    detail: ''
+  },
+  {
+    officialLang: '作战集团',
+    normalLang: '亦称作战集群。为遂行作战任务，对战役编成内的各种力量进行临时组合而形成的较大规模的兵力编组。按类型，分为进攻作战集团和防御作战集团；按行动空间，分为陆上作战集团、海上作战集团和空中作战集团；按军兵种，分为陆军作战集团、海军作战集团、空军作战集团和第二炮兵作战集团等。可按方位、指挥员姓氏或代号等命名。',
+    detail: ''
+  },
+  {
+    officialLang: '军区',
+    normalLang: '亦称大军区。按战略区域设立的军队一级组织。为战略性战役军团。是中央军事委员会直接领导下负责所属部队和单位军事、政治、后勤、装备工作的领导机构。下辖战斗部队、保障部队和省军区等，有的还辖卫戍区、警备区、院校等。',
+    detail: ''
+  },
+  {
+    officialLang: '前送',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '后送',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '程序制导',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '波束制导',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '氢弹',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '中子弹',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '原子弹',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '助爆型原子弹',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '核武器杀伤破坏效应',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '核爆炸光辐射效应',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '早期核辐射效应',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '炮兵测地车',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '装甲密钥加注车',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '航空兵团',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '预备役',
+    normalLang: '公民按照法律规定在现役之外所服的兵役。包括军官预备役和士兵预备役。',
+    detail: ''
+  },
+  {
+    officialLang: '预编士兵',
+    normalLang: '预编到现役部队和编入预备役部队的预备役士兵。',
+    detail: ''
+  },
+  {
+    officialLang: '国防动员能力',
+    normalLang: '国家组织调动各种社会资源保障国防和军事斗争需要的能力。主要由国防动员组织力、国防动员潜力构成。',
+    detail: ''
+  },
+  {
+    officialLang: '国防经济实力',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '全民国防教育日',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '次生灾害',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '城市防护',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '战争因素',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '战争全局',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '战争空间',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '作战原则',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '宿营警戒',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '战斗警戒',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '行军进阶',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '战场勤务',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '网络战场',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '电磁战场',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '海战场',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '大休息',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '小休息',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '调整地区',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '进攻战役',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '战略依托',
+    normalLang: '实现战略目的可以依靠的力量、资源和空间等的统称。',
+    detail: ''
+  },
+  {
+    officialLang: '登陆工具',
+    normalLang: '用于输送登陆部队突击上路的各类舰艇、船、两栖车辆、运输机、直升机等的统称。',
+    detail: ''
+  },
+  {
+    officialLang: '指挥手段',
+    normalLang: '实施指挥所使用的各种装备、器材及方法的统称。包括指挥信息系统及指挥作业工具、指挥信息传递工具和通信方法等。',
+    detail: ''
+  },
+  {
+    officialLang: '军人素质',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '军队建设',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '多样化军事任务',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '军事学术',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '国防力量',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '武装力量建设',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '国防现代化',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '军用物资',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '海疆',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '海防建设',
+    normalLang: '',
+    detail: ''
+  },
+  {
+    officialLang: '民兵武器装备',
+    normalLang: '',
+    detail: ''
+  },
 ]
 
